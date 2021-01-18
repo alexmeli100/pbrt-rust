0b63226eaa8cc4b873ca135633671eaa21c84549
alexmeli100 <alexmeli100@gmail.com>
2021-01-18 18:21:50 -0500

    implemented fourier material

---
+++ src/materials/fourier.rs
@@ -0,0 +1,62 @@
+use crate::core::reflection::{FourierBSDFTable, BSDF, FourierBSDF};
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::collections::HashMap;
+use lazy_static::lazy_static;
+use crate::core::texture::{TextureFloat};
+use crate::core::material::{Material, TransportMode, bump, Materials};
+use bumpalo::Bump;
+use crate::core::interaction::SurfaceInteraction;
+use crate::core::paramset::TextureParams;
+
+lazy_static! {
+    static ref LOADED_BSDFS: Mutex<HashMap<String, Arc<FourierBSDFTable>>> = Mutex::new(HashMap::new());
+}
+
+pub struct FourierMaterial {
+    table   : Arc<FourierBSDFTable>,
+    bump_map: Option<Arc<TextureFloat>>
+}
+
+impl FourierMaterial {
+    pub fn new(filename: &str, bump_map: Option<Arc<TextureFloat>>) -> Self {
+        let mut loaded = LOADED_BSDFS.lock().unwrap();
+
+        if !loaded.contains_key(filename) {
+            let mut table = FourierBSDFTable::default();
+            FourierBSDFTable::read(filename, &mut table);
+            loaded.insert(filename.to_owned(), Arc::new(table));
+        }
+
+        Self {
+            bump_map,
+            table: loaded.get(filename).unwrap().clone()
+        }
+    }
+}
+
+impl Material for FourierMaterial {
+    fn compute_scattering_functions<'a: 'a>(
+        &self, si: &mut SurfaceInteraction<'a>, arena: &'a Bump,
+        mode: TransportMode, _allow_multiple_lobes: bool) {
+        // Perform bump mapping with bumpMap if present
+        if self.bump_map.is_some() { bump(self.bump_map.as_ref().unwrap(), si); }
+
+        let bsdf = arena.alloc(BSDF::new(si, 1.0));
+
+        // Checking for zero channels works as a proxy for checking whether the
+        // table was successfully read from the file
+        if self.table.nchannels > 0 {
+            bsdf.add(arena.alloc(FourierBSDF::new(self.table.clone(), mode).into()));
+        }
+
+        si.bsdf = Some(bsdf)
+    }
+}
+
+pub fn create_fourier_material(mp: &mut TextureParams) -> Materials {
+    let bump_map = mp.get_floattexture_ornull("bumpmap");
+    let filename = mp.find_filename("bsdffile", "");
+
+    FourierMaterial::new(&filename, bump_map).into()
+}
