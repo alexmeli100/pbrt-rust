7868b2d5c6494c99bc3f18d02832e414c449e2fe
alexmeli100 <alexmeli100@gmail.com>
2021-01-18 18:21:18 -0500

    some additional methods

--- src/core/geometry/geometry.rs
+++ src/core/geometry/geometry.rs
@@ -1,5 +1,5 @@
 use crate::core::geometry::point::Point3f;
-use crate::core::geometry::vector::Vector3f;
+use crate::core::geometry::vector::{Vector3f, Vector3};
 use crate::core::geometry::normal::Normal3f;
 use crate::core::pbrt::*;
 
@@ -21,4 +21,33 @@ pub fn offset_ray_origin(p: &Point3f, p_error: &Vector3f, n: &Normal3f, w: &Vect
     }
 
     po
+}
+
+#[inline]
+pub fn spherical_direction(sin_theta: Float, cos_theta: Float, phi: Float) -> Vector3f {
+    Vector3f::new(
+        sin_theta * phi.cos(),
+        sin_theta * phi.sin(),
+        cos_theta
+    )
+}
+
+#[inline]
+pub fn spherical_direction_basis(sin_theta: Float, cos_theta: Float, phi: Float, x: &Vector3f, y: &Vector3f, z: &Vector3f) -> Vector3f {
+    *x * sin_theta * phi.cos() + *y * sin_theta * phi.sin() + *z * cos_theta
+}
+#[inline]
+pub fn spherical_theta(v: &Vector3f) -> Float {
+    clamp(v.z, -1.0, -1.0).acos()
+}
+
+#[inline]
+pub fn spherical_phi(v: &Vector3f) -> Float {
+    let p = v.y.atan2(v.x);
+
+    if p < 0.0 {
+        p + 2.0 * PI
+    } else {
+        p
+    }
 }
