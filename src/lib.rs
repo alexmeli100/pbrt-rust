e0bd861f79ad9846ca302e7007737b7d0b94e66e
alexmeli100 <alexmeli100@gmail.com>
2021-01-18 18:47:02 -0500

    additional modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,33 @@
+#![feature(iter_partition_in_place)]
+#![feature(slice_partition_at_index)]
+#![feature(const_fn)]
+#![allow(incomplete_features)]
+#![feature(const_generics)]
+
+// lints
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+
+// clippy
+#![cfg_attr(
+    feature = "cargo-clippy",
+    allow(
+        clippy::many_single_char_names,
+        clippy::too_many_arguments
+    )
+)]
+
 pub mod core;
 pub mod materials;
 pub mod shapes;
+pub mod accelerators;
+pub mod cameras;
+pub mod samplers;
+pub mod textures;
+pub mod filters;
+pub mod pbrtparser;
+pub mod media;
+pub mod lights;
+
 
