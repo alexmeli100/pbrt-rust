db06b9e3ca355a5a2192143c7f6535c95d582d20
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 22:26:28 -0500

    modules for shape

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -2,3 +2,12 @@ pub mod sphere;
 pub mod cylinder;
 pub mod disk;
 pub mod triangle;
+pub mod plymesh;
+pub mod nurbs;
+pub mod curve;
+pub mod loopsubdiv;
+pub mod heightfield;
+pub mod hyperboloid;
+pub mod paraboloid;
+pub mod cone;
+
