d3522521bf6aa4825811a4512e028593d1f4bf95
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 22:19:19 -0500

    bug fixes

--- src/core/primitive.rs
+++ src/core/primitive.rs
@@ -1,12 +1,12 @@
 use enum_dispatch::enum_dispatch;
 use crate::core::geometry::bounds::Bounds3f;
 use crate::core::geometry::ray::Ray;
-use crate::core::interaction::{SurfaceInteraction, Interaction};
+use crate::core::interaction::{SurfaceInteraction};
 use crate::core::material::Material;
 use crate::core::material::{Materials, TransportMode};
 use std::sync::Arc;
-use crate::core::light::AreaLights;
-use bumpalo::Bump;
+use crate::core::light::{Lights};
+use bumpalo_herd::Member;
 use crate::core::transform::{AnimatedTransform, Transform};
 use crate::core::shape::{Shape, Shapes};
 use crate::core::medium::MediumInterface;
@@ -16,15 +16,18 @@ use crate::accelerators::kdtreeaccel::KdTreeAccel;
 #[enum_dispatch]
 pub trait Primitive {
     fn world_bound(&self) -> Bounds3f;
-    fn intersect(&self, r: &mut Ray, s: &mut SurfaceInteraction) -> bool;
+    fn intersect(
+        &self, r: &mut Ray,
+        s: &mut SurfaceInteraction,
+        p: Arc<Primitives>) -> bool;
+
     fn intersect_p(&self, r: &mut Ray) -> bool;
     fn get_material(&self) -> Option<Arc<Materials>>;
-    fn get_area_light(&self) -> Option<Arc<AreaLights>>;
-    fn compute_scattering_functions<'a: 'a>(
-        &self, isect: &mut SurfaceInteraction<'a>, arena: &'a Bump,
+    fn get_area_light(&self) -> Option<Arc<Lights>>;
+    fn compute_scattering_functions<'b: 'b>(
+        &self, isect: &mut SurfaceInteraction<'b>, arena: &Member<'b>,
          mode: TransportMode, allow_multiple_lobes: bool);
 }
-
 
 
 #[enum_dispatch(Primitive)]
@@ -52,13 +55,16 @@ impl Primitive for TransformedPrimitive {
         self.prim_to_world.motion_bounds(&self.primitive.world_bound())
     }
 
-    fn intersect(&self, r: &mut Ray, isect: &mut SurfaceInteraction) -> bool {
+    fn intersect(
+        &self, r: &mut Ray,
+        isect: &mut SurfaceInteraction,
+        _p: Arc<Primitives>) -> bool {
         // Compute ray after transformation by PrimitiveToWorld
         let mut inter_prim_toworld = Transform::default();
         self.prim_to_world.interpolate(r.time, &mut inter_prim_toworld);
         let mut ray = Transform::inverse(&inter_prim_toworld).transform_ray(r);
 
-        if !self.primitive.intersect(&mut ray, isect) {
+        if !self.primitive.intersect(&mut ray, isect, self.primitive.clone()) {
             return false;
         }
 
@@ -85,12 +91,12 @@ impl Primitive for TransformedPrimitive {
         None
     }
 
-    fn get_area_light(&self) -> Option<Arc<AreaLights>> {
+    fn get_area_light(&self) -> Option<Arc<Lights>> {
         None
     }
 
-    fn compute_scattering_functions<'a: 'a>(
-        &self, _isect: &mut SurfaceInteraction<'a>, _arena: &'a Bump,
+    fn compute_scattering_functions<'b: 'b>(
+        &self, _isect: &mut SurfaceInteraction<'b>, _arena: &Member<'b>,
         _mode: TransportMode, _allow_multiple_lobes: bool) {
         panic!("TransformedPrimitive::compute_scattering_functions() shouldn't be called")
     }
@@ -100,15 +106,14 @@ impl Primitive for TransformedPrimitive {
 pub struct GeometricPrimitive {
     shape               : Arc<Shapes>,
     material            : Option<Arc<Materials>>,
-    area_light          : Option<Arc<AreaLights>>,
+    area_light          : Option<Arc<Lights>>,
     medium_interface    : MediumInterface
 }
 
 impl GeometricPrimitive {
     pub fn new(
         shape: Arc<Shapes>, material: Option<Arc<Materials>>,
-        area_light: Option<Arc<AreaLights>>,
-        medium_interface: MediumInterface) -> Self {
+        area_light: Option<Arc<Lights>>, medium_interface: MediumInterface) -> Self {
         Self { shape, material, area_light, medium_interface }
     }
 }
@@ -118,15 +123,19 @@ impl Primitive for GeometricPrimitive {
         self.shape.world_bound()
     }
 
-    fn intersect(&self, r: &mut Ray, isect: &mut SurfaceInteraction) -> bool {
+    fn intersect(
+        &self, r: &mut Ray,
+        isect: &mut SurfaceInteraction,
+        p: Arc<Primitives>) -> bool {
         let mut thit = 0.0;
 
-        if !self.shape.intersect(r, &mut thit, isect, true) {
+        let s = Some(self.shape.clone());
+        if !self.shape.intersect(r, &mut thit, isect, true, s) {
             return false;
         }
 
         r.t_max = thit;
-        isect.primitive = Some(Arc::new(self.clone().into()));
+        isect.primitive = Some(p);
         assert!(isect.n.dot(&isect.shading.n) >= 0.0);
 
         let m = match self.medium_interface.is_medium_transition() {
@@ -147,17 +156,17 @@ impl Primitive for GeometricPrimitive {
         self.material.clone()
     }
 
-    fn get_area_light(&self) -> Option<Arc<AreaLights>> {
+    fn get_area_light(&self) -> Option<Arc<Lights>> {
         self.area_light.clone()
     }
 
-    fn compute_scattering_functions<'a: 'a>(
-        &self, isect: &mut SurfaceInteraction<'a>, arena: &'a Bump,
-        mode: TransportMode, allow_multiple_lobes: bool) {
+    fn compute_scattering_functions<'b: 'b>(
+        &self, isect: &mut SurfaceInteraction<'b>, arena: &Member<'b>,
+         mode: TransportMode, allow_multiple_lobes: bool) {
         // TODO: ProfilePhase
 
         if let Some(m) = &self.material {
-            m.compute_scattering_functions(isect, arena, mode, allow_multiple_lobes);
+            m.compute_scattering_functions(isect, arena, Some(m.clone()), mode, allow_multiple_lobes);
         }
 
         assert!(isect.n.dot(&isect.shading.n) >= 0.0)
