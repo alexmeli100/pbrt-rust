a7668632a25fec877f5ddb6c4c587d54c272e855
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 22:04:23 -0500

    Refactorings and bug fixes

--- src/core/rng.rs
+++ src/core/rng.rs
@@ -1,10 +1,11 @@
 use hexf::*;
 use crate::core::pbrt::Float;
 
-pub const ONE_MINUS_EPSILON: Float = hexf32!("0x1.fffffep-1");
-pub const PCG32_DEFAULT_STATE: u64 = 0x853c_49e6_748f_ea9b;
-pub const PCG32_DEFAULT_STREAM: u64 = 0xda3e_39cb_94b9_5bdb;
-pub const PCG32_MULT: u64 = 0x5851_f42d_4c95_7f2d;
+pub const FLOAT_ONE_MINUS_EPSILON   : Float = hexf32!("0x1.fffffep-1");
+pub const ONE_MINUS_EPSILON         : Float = FLOAT_ONE_MINUS_EPSILON;
+pub const PCG32_DEFAULT_STATE       : u64 = 0x853c_49e6_748f_ea9b;
+pub const PCG32_DEFAULT_STREAM      : u64 = 0xda3e_39cb_94b9_5bdb;
+pub const PCG32_MULT                : u64 = 0x5851_f42d_4c95_7f2d;
 
 #[derive(Debug, Copy, Clone)]
 pub struct RNG {
@@ -30,12 +31,21 @@ impl RNG {
     }
 
     pub fn uniform_int32(&mut self) -> u32 {
-        let old_state = self.state;
-        self.state = old_state * PCG32_MULT + self.inc;
-        let xor_shifted = (((old_state >> 18) ^ old_state) >> 27) as u32;
-        let rot = (old_state >> 59) as u32;
-
-        (xor_shifted >> rot) | (xor_shifted << ((!rot + 1) & 31))
+        let oldstate: u64 = self.state;
+        let (mul, _) = oldstate.overflowing_mul(PCG32_MULT);
+        let (add, _) = mul.overflowing_add(self.inc);
+        self.state = add;
+        let (shr, _) = oldstate.overflowing_shr(18);
+        let combine = shr ^ oldstate;
+        let (shr, _) = combine.overflowing_shr(27);
+        let xorshifted: u32 = shr as u32;
+        let (shr, _) = oldstate.overflowing_shr(59);
+        let rot: u32 = shr as u32;
+        let (shr, _overflow) = xorshifted.overflowing_shr(rot);
+        let neg = !rot;
+        let (add, _) = neg.overflowing_add(1_u32);
+        let (shl, _) = xorshifted.overflowing_shl(add & 31);
+        shr | shl
     }
 
     pub fn uniform_int32_2(&mut self, b: u32) -> u32 {
@@ -43,10 +53,10 @@ impl RNG {
 
         loop {
             let r = self.uniform_int32();
-
             if r >= threshold {
                 return r % b;
             }
+
         }
     }
 
@@ -54,11 +64,13 @@ impl RNG {
         ONE_MINUS_EPSILON.min(self.uniform_int32() as Float * hexf32!("0x1.0p-32"))
     }
 
-    pub fn set_sequence(&mut self, init_seq: u64) {
+    pub fn set_sequence(&mut self, initseq: u64) {
         self.state = 0;
-        self.inc = (init_seq << 1) | 1;
+        let (shl, _) = initseq.overflowing_shl(1);
+        self.inc = shl | 1;
         self.uniform_int32();
-        self.state += PCG32_DEFAULT_STATE;
+        let (add, _) = self.state.overflowing_add(PCG32_DEFAULT_STATE);
+        self.state = add;
         self.uniform_int32();
     }
 }
