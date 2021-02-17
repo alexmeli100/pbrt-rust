5554017ac8f8d0c95829743af5dbb7d646d210aa
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 21:32:50 -0500

    additional trait methods implemented

--- src/lights/goniometric.rs
+++ src/lights/goniometric.rs
@@ -5,15 +5,19 @@ use crate::core::spectrum::{Spectrum, RGBSpectrum, SpectrumType};
 use crate::core::mipmap::MIPMap;
 use crate::mipmap;
 use crate::init_light_data;
-use crate::core::light::{LightFlags, Light, VisibilityTester};
+use crate::core::light::{LightFlags, Light, VisibilityTester, Lights};
 use crate::core::imageio::read_image;
 use crate::core::geometry::vector::Vector3f;
 use crate::core::geometry::geometry::{spherical_theta, spherical_phi};
-use crate::core::pbrt::{INV2_PI, INV_PI, Float, PI};
+use crate::core::pbrt::{INV2_PI, INV_PI, Float, PI, INFINITY};
 use crate::core::geometry::ray::Ray;
-use crate::core::interaction::{Interactions, SurfaceInteraction, Interaction};
+use crate::core::interaction::{Interaction, InteractionData};
 use crate::core::geometry::normal::Normal3f;
+use crate::core::paramset::ParamSet;
+use std::sync::Arc;
+use crate::core::sampling::{uniform_sample_sphere, uniform_sphere_pdf};
 
+#[allow(dead_code)]
 #[derive(Default)]
 pub struct GonioPhotometricLight {
     plight           : Point3f,
@@ -74,48 +78,62 @@ impl Light for GonioPhotometricLight {
         self.I * s * 4.0 * PI
     }
 
-    fn le(&self, r: &Ray) -> Spectrum {
-        unimplemented!()
-    }
-
-    fn pdf_li(&self, re: &Interactions, wi: &Vector3f) -> f32 {
+    fn pdf_li(&self, _re: &InteractionData, _wi: &Vector3f) -> Float {
         0.0
     }
 
     fn sample_li(
-        &self, re: &Interactions, u: &Point2f, wi: &mut Vector3f,
+        &self, re: &InteractionData, _u: &Point2f, wi: &mut Vector3f,
         pdf: &mut Float, vis: &mut VisibilityTester) -> Spectrum {
         // TODO: ProfilePhase
         *wi = (self.plight - re.p()).normalize();
         *pdf = 1.0;
-        let s1 = SurfaceInteraction {
-            p       : re.p(),
-            n       : re.n(),
-            time    : re.time(),
-            p_error : re.p_error(),
-            wo      : re.wo(),
-            ..Default::default()
 
-        };
-        let s2 = SurfaceInteraction {
+        let s2 = InteractionData {
             p               : self.plight,
             time            : re.time(),
             medium_interface: Some(self.medium_interface.clone()),
             ..Default::default()
         };
 
-        *vis = VisibilityTester::new(s1.into(), s2.into());
+        *vis = VisibilityTester::new(re.clone(), s2);
+
 
         self.I * self.scale(&(-*wi)) / self.plight.distance_squared(&re.p())
     }
 
     fn sample_le(
-        &self, u1: &Point2f, u2: &Point2f, time: Float, ray: &mut Ray,
+        &self, u1: &Point2f, _u2: &Point2f, time: Float, ray: &mut Ray,
         nlight: &mut Normal3f, pdf_pos: &mut Float, pdf_dir: &mut Float) -> Spectrum {
-        unimplemented!()
+        // TODO: ProfilePhase
+        *ray = Ray::new(
+            &self.plight, &uniform_sample_sphere(u1), INFINITY,
+            time, self.medium_interface.inside.clone(), None);
+        *nlight = Normal3f::from(ray.d);
+        *pdf_pos = 1.0;
+        *pdf_dir = uniform_sphere_pdf();
+
+        self.I * self.scale(&ray.d)
     }
 
-    fn pdf_le(&self, ray: &Ray, nlight: &Normal3f, pdf_pos: &mut f32, pdf_dir: &mut f32) {
-        unimplemented!()
+    fn pdf_le(&self, _ray: &Ray, _nlight: &Normal3f, pdf_pos: &mut Float, pdf_dir: &mut Float) {
+        // TODO ProfilePhase
+        *pdf_pos = 0.0;
+        *pdf_dir = uniform_sphere_pdf();
     }
+
+    fn nsamples(&self) -> usize { self.nsamples }
+
+    fn flags(&self) -> u8 {
+        self.flags
+    }
+}
+
+pub fn create_goniometriclight(l2w: &Transform, mi: MediumInterface, params: &ParamSet) -> Option<Arc<Lights>> {
+    let I = params.find_one_spectrum("I", Spectrum::new(1.0));
+    let sc = params.find_one_spectrum("sc", Spectrum::new(1.0));
+    let texname = params.find_one_filename("mapname", "");
+    let l = GonioPhotometricLight::new(l2w, mi, &(I * sc), &texname);
+
+    Some(Arc::new(l.into()))
 }
