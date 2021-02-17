3a9af1d5849b10a1f5bec5faa4e8aeeb049a1c0f
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 22:25:37 -0500

    added create method for Orthographic Camera

--- src/cameras/orthographic.rs
+++ src/cameras/orthographic.rs
@@ -1,21 +1,19 @@
 use crate::core::geometry::vector::Vector3f;
 use crate::core::transform::{Transform, AnimatedTransform};
 use crate::core::pbrt::{Float, INFINITY, lerp};
-use crate::core::camera::{Camera, CameraSample};
-use crate::init_projective_camera;
-use crate::init_camera;
-use crate::core::light::VisibilityTester;
-use crate::core::geometry::point::{Point2f, Point3f};
+use crate::core::camera::{Camera, CameraSample, Cameras};
+use crate::core::geometry::point::{Point3f};
 use crate::core::geometry::ray::{Ray, RayDifferential};
-use crate::core::interaction::Interactions;
-use crate::core::spectrum::Spectrum;
 use crate::core::geometry::bounds::Bounds2f;
 use crate::core::film::Film;
 use std::sync::Arc;
 use crate::core::medium::Mediums;
+use crate::get_camera_data;
 use crate::core::sampling::concentric_sample_disk;
+use crate::core::paramset::ParamSet;
+use log::{warn, error};
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub struct OrthographicCamera {
     pub shutter_open        : Float,
     pub shutter_close       : Float,
@@ -28,33 +26,50 @@ pub struct OrthographicCamera {
     pub raster_to_screen    : Transform,
     pub lens_radius         : Float,
     pub focal_distance      : Float,
-    pub film                : Option<Arc<Film>>,
+    pub film                : Arc<Film>,
     pub medium              : Option<Arc<Mediums>>
 
 }
 
 impl OrthographicCamera {
-    fn new(
+    pub fn new(
         camera_to_world: AnimatedTransform, screen_window: &Bounds2f,
         shutter_open: Float, shutter_close: Float, lens_radius: Float,
-        focal_distance: Float, film: Option<Arc<Film>>, medium: Option<Arc<Mediums>>
+        focal_distance: Float, film: Arc<Film>, medium: Option<Arc<Mediums>>
     ) -> Self {
-        let mut camera = OrthographicCamera::default();
         let camera_to_screen = Transform::orthographic(0.0, 1.0);
 
-        init_projective_camera!(
-            camera, camera_to_world, camera_to_screen, screen_window,
-            shutter_open, shutter_close, lens_radius, focal_distance, film, medium
-        );
+        // Compute projective camera transformations
 
-        camera.dx_camera = camera.raster_to_camera.transform_vector(&Vector3f::new(1.0, 0.0, 0.0));
-        camera.dy_camera = camera.raster_to_camera.transform_vector(&Vector3f::new(0.0, 1.0, 0.0));
+        // Compute projective camera screen transformations
 
-        camera
+        let screen_to_raster =
+            Transform::scale(film.full_resolution.x as Float, film.full_resolution.y as Float, 1.0) *
+                Transform::scale(
+                    1.0 / (screen_window.p_max.x - screen_window.p_min.x),
+                    1.0 / (screen_window.p_min.y - screen_window.p_max.y),
+                    1.0) *
+                Transform::translate(&Vector3f::new(-screen_window.p_min.x, -screen_window.p_max.y, 0.0));
+
+
+        let raster_to_screen = Transform::inverse(&screen_to_raster);
+        let raster_to_camera = Transform::inverse(&camera_to_screen) * raster_to_screen;
+
+        let dx_camera = raster_to_camera.transform_vector(&Vector3f::new(1.0, 0.0, 0.0));
+        let dy_camera = raster_to_camera.transform_vector(&Vector3f::new(0.0, 1.0, 0.0));
+
+        OrthographicCamera {
+            film, medium, shutter_open, shutter_close,
+            dx_camera, dy_camera, camera_to_screen,
+            camera_to_world, raster_to_screen, screen_to_raster,
+            raster_to_camera, focal_distance, lens_radius,
+        }
     }
 }
 
 impl Camera for OrthographicCamera {
+    get_camera_data!();
+
     fn generate_ray(&self, sample: &CameraSample, r: &mut Ray) -> f32 {
         // Compute raster and camera sample positions
         let pfilm = Point3f::new(sample.pfilm.x, sample.pfilm.y, 0.0);
@@ -135,21 +150,58 @@ impl Camera for OrthographicCamera {
 
         1.0
     }
+}
 
-    fn we(&self, ray: &Ray, p_raster2: Option<&[Point2f]>) {
-        // Compute main orthographic viewing ray
+pub fn create_orthographic_camera(
+    params: &ParamSet, cam2world: AnimatedTransform,
+    film: Arc<Film>, medium: Option<Arc<Mediums>>) -> Option<Arc<Cameras>> {
+    // Extract common camera parameters from ParamSet
+    let mut shutteropen = params.find_one_float("shutteropen", 0.0);
+    let mut shutterclose = params.find_one_float("shutterclose", 0.0);
 
-        // Compute raster and camera sample positions
-
+    if shutterclose < shutteropen {
+        warn!("Shutter close time [{}] < shutter open [{}]. Swapping time.",
+              shutterclose, shutteropen);
+        std::mem::swap(&mut shutteropen, &mut shutterclose);
     }
 
-    fn pdf_we(&self, ray: &Ray, pdf_pos: &[f32], pdf_dir: &[f32]) {
-        unimplemented!()
+    let lensradius = params.find_one_float("lensradius", 0.0);
+    let focaldistance = params.find_one_float("focaldistance", 1.0e30);
+    let frame = params.find_one_float(
+        "frameaspectratio",
+        film.full_resolution.x as Float / film.full_resolution.y as Float);
+
+    let mut screen = Bounds2f::default();
+
+    if frame > 1.0 {
+        screen.p_min.x = -frame;
+        screen.p_max.x = frame;
+        screen.p_min.y = -1.0;
+        screen.p_max.y = 1.0;
+    } else {
+        screen.p_min.x = -1.0;
+        screen.p_max.x = 1.0;
+        screen.p_min.y = -1.0 / frame;
+        screen.p_max.y = 1.0 / frame;
     }
 
-    fn sample_wi(
-        &self, r: &Interactions, u: &Point2f, wi: &mut Vector3f,
-        pdf: &mut [f32], p_raster: &mut [Point2f], vis: &mut VisibilityTester) -> Spectrum {
-        unimplemented!()
+    let mut swi = 0;
+    let sw = params.find_float("screenwindow", &mut swi);
+
+    if let Some(ref s) = sw {
+        if swi == 4 {
+            screen.p_min.x = s[0];
+            screen.p_max.x = s[1];
+            screen.p_min.y = s[2];
+            screen.p_max.y = s[3];
+        } else {
+            error!("\"screenwindow\" should have four values");
+        }
     }
+
+    let cam = OrthographicCamera::new(
+        cam2world, &screen, shutteropen, shutterclose,
+        lensradius, focaldistance, film, medium);
+
+    Some(Arc::new(cam.into()))
 }
