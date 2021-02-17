3999341b8f573b77df72a2393c40e8d09274d237
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 22:31:03 -0500

    switched to bumpalo_herd for arena

--- src/materials/matte.rs
+++ src/materials/matte.rs
@@ -3,11 +3,11 @@ use crate::core::texture::{Texture, TextureSpec, TextureFloat};
 use crate::core::spectrum::Spectrum;
 use crate::core::pbrt::{clamp, INFINITY};
 use crate::core::material::{Material, TransportMode, bump, Materials};
-use bumpalo::Bump;
 use crate::core::interaction::SurfaceInteraction;
 use crate::core::reflection::{BSDF, BxDFs, OrenNayar};
 use crate::core::reflection::{LambertianReflection};
 use crate::core::paramset::TextureParams;
+use bumpalo_herd::Member;
 
 pub struct MatteMaterial {
     kd      : Arc<TextureSpec>,
@@ -25,14 +25,14 @@ impl MatteMaterial {
 }
 
 impl Material for MatteMaterial {
-    fn compute_scattering_functions<'a: 'a>(
-        &self, si: &mut SurfaceInteraction<'a>, arena: &'a Bump,
-        _mode: TransportMode, _allow_multiple_lobes: bool) {
+    fn compute_scattering_functions<'b: 'b>(
+        &self, si: &mut SurfaceInteraction<'b>, arena: &Member<'b>,
+        _mat: Option<Arc<Materials>>, _mode: TransportMode, _allow_multiple_lobes: bool) {
         // Perform bump mapping with bumpMap if present
         if self.bump_map.is_some() { bump(self.bump_map.as_ref().unwrap(), si); }
 
         // Evaluate textures for MatteMaterial and allocate BRDF
-        let mut bsdf = arena.alloc(BSDF::new(si, 1.0));
+        let bsdf = arena.alloc(BSDF::new(si, 1.0));
         let r = self.kd.evaluate(si).clamps(0.0, INFINITY);
         let sig = clamp(self.sigma.evaluate(si), 0.0 , 90.0);
 
