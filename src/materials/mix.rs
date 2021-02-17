37d3e8ac455a0afaba00af5470cd6991bd5e421d
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 22:28:07 -0500

    switched to bumpalo_herd for arena

--- src/materials/mix.rs
+++ src/materials/mix.rs
@@ -1,12 +1,12 @@
 use crate::core::material::{Materials, Material, TransportMode};
 use std::sync::Arc;
-use crate::core::texture::{Textures, Texture, TextureSpec};
+use crate::core::texture::{Texture, TextureSpec};
 use crate::core::spectrum::Spectrum;
-use bumpalo::Bump;
 use crate::core::interaction::SurfaceInteraction;
 use crate::core::pbrt::INFINITY;
 use crate::core::reflection::{BxDFType, ScaledBxDF};
 use crate::core::paramset::TextureParams;
+use bumpalo_herd::Member;
 
 pub struct MixMaterial {
     m1      : Arc<Materials>,
@@ -23,22 +23,22 @@ impl MixMaterial {
 }
 
 impl Material for MixMaterial {
-    fn compute_scattering_functions<'a: 'a>(
-        &self, si: &mut SurfaceInteraction<'a>, arena: &'a Bump,
-        mode: TransportMode, allow_multiple_lobes: bool) {
+    fn compute_scattering_functions<'b: 'b>(
+        &self, si: &mut SurfaceInteraction<'b>, arena: & Member<'b>,
+        _mat: Option<Arc<Materials>>, mode: TransportMode, allow_multiple_lobes: bool) {
         let s1 = self.scale.evaluate(si).clamps(0.0, INFINITY);
         let s2 = (Spectrum::new(1.0) - s1).clamps(0.0, INFINITY);
         let mut si2 = SurfaceInteraction::new(
             &si.p, &si.p_error, &si.uv, &si.wo, &si.dpdu, &si.dpdv,
             &si.dndu, &si.dndv, si.time, si.shape.clone());
 
-        self.m1.compute_scattering_functions(si, arena, mode, allow_multiple_lobes);
-        self.m2.compute_scattering_functions(&mut si2, arena, mode, allow_multiple_lobes);
+        self.m1.compute_scattering_functions(si, arena, Some(self.m1.clone()), mode, allow_multiple_lobes);
+        self.m2.compute_scattering_functions(&mut si2, arena, Some(self.m2.clone()), mode, allow_multiple_lobes);
 
         let b1 = si.bsdf.as_mut().unwrap();
         let b2 = si2.bsdf.as_mut().unwrap();
-        let n1 = b1.num_components(BxDFType::All);
-        let n2 = b2.num_components(BxDFType::All);
+        let n1 = b1.num_components(BxDFType::All as u8);
+        let n2 = b2.num_components(BxDFType::All as u8);
 
         for i in 0..n1 {
             b1.bxdfs[i] = arena.alloc(ScaledBxDF::new(b1.bxdfs[i], &s1).into());
