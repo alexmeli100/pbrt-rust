1939e3778353760933e6e353f67cd3b88ba8b644
alexmeli100 <alexmeli100@gmail.com>
2021-02-16 21:53:45 -0500

    additional trait implementations

--- src/media/homogeneous.rs
+++ src/media/homogeneous.rs
@@ -1,11 +1,12 @@
 use crate::core::spectrum::Spectrum;
 use crate::core::pbrt::{Float};
-use crate::core::medium::Medium;
-use crate::core::sampler::Samplers;
+use crate::core::medium::{Medium, Mediums, PhaseFunctions, HenyeyGreenstein, MediumInterface};
+use crate::core::sampler::{Sampler};
 use crate::core::interaction::MediumInteraction;
 use crate::core::geometry::ray::Ray;
+use std::sync::Arc;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct HomogeneousMedium {
     g       : Float,
     sigma_t : Spectrum,
@@ -25,12 +26,45 @@ impl HomogeneousMedium {
 }
 
 impl Medium for HomogeneousMedium {
-    fn tr(&self, ray: &Ray, sampler: &mut Samplers) -> Spectrum {
+    fn tr<S: Sampler>(&self, ray: &Ray, _sampler: &mut S) -> Spectrum {
         // TODO: ProfilePhase
         (-self.sigma_t * (ray.t_max * ray.d.length()).min(f32::MAX)).exp()
     }
 
-    fn sample(&self, ray: &Ray, sampler: &mut Samplers, mi: &mut MediumInteraction) -> Spectrum {
-        unimplemented!()
+    fn sample<S: Sampler>(
+        &self, ray: &Ray, sampler: &mut S,
+        mi: &mut MediumInteraction) -> Spectrum {
+        // Todo: ProfilePhase
+        // Sample a channel and distance along the ray;
+        let channel = std::cmp::min(
+            sampler.get_1d() as usize * Spectrum::n(),
+            Spectrum::n() - 1);
+        let dist = -((1.0 - sampler.get_1d()).ln()) / self.sigma_t[channel];
+        let t = (dist / ray.d.length()).min(ray.t_max);
+        let sampled_medium = t < ray.t_max;
+
+        if sampled_medium {
+            let medium: Option<Arc<Mediums>> = Some(Arc::new((*self).into()));
+            let pf: Option<PhaseFunctions> = Some(HenyeyGreenstein::new(self.g).into());
+            let minter = Some(MediumInterface::new(medium));
+            *mi = MediumInteraction::new(
+                &ray.find_point(t), &(-ray.d),
+                ray.time, minter, pf);
+        }
+
+        // Compute the transmittance and sampling for scattering from homogeneous medium
+        let Tr = (-self.sigma_t * t.min(f32::MAX) * ray.d.length()).exp();
+
+        // Return weighting factor for scattering from homogeneous medium
+        let density = if sampled_medium { self.sigma_t * Tr } else { Tr };
+        let mut pdf = 0.0;
+        for i in 0..Spectrum::n() { pdf += density[i]; }
+        pdf *= 1.0 / Spectrum::n() as Float;
+        if pdf == 0.0 {
+            assert!(Tr.is_black());
+            pdf = 1.0;
+        }
+
+        if sampled_medium { Tr * self.sigma_s / pdf } else { Tr / pdf }
     }
 }
